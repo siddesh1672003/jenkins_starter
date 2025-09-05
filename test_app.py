19a22cd043765fb8fcf9cd6612c62cbbff92c587
I578428 <siddesh.patil@sap.com>
2025-09-05 15:12:07 +0530

    updated app and test files

---
+++ test_app.py
@@ -0,0 +1,9 @@
+import unittest
+import app
+
+class TestApp(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(app.add(2, 3), 5)
+
+if __name__ == '__main__':
+    unittest.main()
